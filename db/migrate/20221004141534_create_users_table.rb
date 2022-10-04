4c91f61d580f61e3715279b3faea0d72c9884e5a
Zachary Casares <rrumble250@gmail.com>
2022-10-04 07:17:11 -0700

    created users table

--- db/migrate/20221004141534_create_users_table.rb
+++ db/migrate/20221004141534_create_users_table.rb
@@ -1,11 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :users_tables do |t|
+    create_table :users do |t|
       t.string :username
-      t.string :email
       t.string :password_digest
-
-      t.timestamps
+      
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_141317) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_141534) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
